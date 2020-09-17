2a410697b5e676f36ddf316cebbc53a4a775ecd2
Luke Brobbin <lbrobbin@jaguarlandrover.com>
2020-09-17 15:38:31 +0100

    excercise 03

--- src/02/index.js
+++ src/02/index.js
@@ -1,20 +1,35 @@
-import { createMachine } from 'xstate';
+import { createMachine } from "xstate";
 
-const elBox = document.querySelector('#box');
+const elBox = document.querySelector("#box");
 
 const machine = createMachine({
-  // Add your object machine definition here
+  initial: "inactive",
+  states: {
+    inactive: {
+      on: {
+        TOGGLE: "active",
+      },
+    },
+    active: {
+      on: {
+        TOGGLE: "inactive",
+      },
+    },
+  },
 });
 
 // Change this to the initial state
-let currentState = undefined;
+let currentState = machine.initial;
 
 function send(event) {
   // Determine and update the `currentState`
+  currentState = machine.transition(currentState, event);
 
+  console.log(currentState);
   elBox.dataset.state = currentState.value;
 }
 
-elBox.addEventListener('click', () => {
+elBox.addEventListener("click", () => {
+  send("TOGGLE");
   // Send a click event
 });
